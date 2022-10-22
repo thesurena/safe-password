e2fcf86275152a50eb285902f45e2eabdb0ab2da
thesurena <91267198+thesurena@users.noreply.github.com>
2022-10-22 06:03:36 +0330

    Add comments

--- Safe-Password.py
+++ Safe-Password.py
@@ -1,16 +1,17 @@
 import string,random
 
-
+#Get a number from the user to generate a random password
 length = int(input('Enter your number to create a strong password: '))
 
 while True:
-
+    #Generate a random password
     chars = string.ascii_letters + string.digits + '!@#$%&()_/'
-
     password = ''.join(random.choice(chars) for itemes in range(length))
 
+    #Show a random password to the user
     print(f'your safe password: {password}')
     
+    #Create or stop the password creation operation
     while True:
         answer = input('do you want another password (Y/n)'.lower())
         if answer == 'y' or answer == 'n':
